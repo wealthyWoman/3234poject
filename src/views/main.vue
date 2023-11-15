f628126a8ed1b84560e99ccf42c58607286758a6
L <3234576207@qq.com>
2023-11-15 21:40:14 +0800

    使用Container组件进行整体的ui搭建

--- src/views/main.vue
+++ src/views/main.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
-    <h1>main页面</h1>
-    <router-view></router-view>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>
+          <!-- <h1>main页面</h1> -->
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
