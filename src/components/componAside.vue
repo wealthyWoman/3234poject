a447b92bb2220b41693fc31cdba019aa18ec07cd
L <3234576207@qq.com>
2023-11-21 22:21:12 +0800

    home页面二级菜单的实现

--- src/components/componAside.vue
+++ src/components/componAside.vue
@@ -1,60 +1,98 @@
 <template>
-  <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose"
-    :collapse="isCollapse">
-    <el-submenu index="1">
-      <template slot="title">
-        <i class="el-icon-location"></i>
-        <span slot="title">导航一</span>
-      </template>
-      <el-menu-item-group>
-        <span slot="title">分组一</span>
-        <el-menu-item index="1-1">选项1</el-menu-item>
-        <el-menu-item index="1-2">选项2</el-menu-item>
-      </el-menu-item-group>
-      <el-menu-item-group title="分组2">
-        <el-menu-item index="1-3">选项3</el-menu-item>
-      </el-menu-item-group>
-      <el-submenu index="1-4">
-        <span slot="title">选项4</span>
-        <el-menu-item index="1-4-1">选项1</el-menu-item>
-      </el-submenu>
-    </el-submenu>
-    <el-menu-item index="2">
-      <i class="el-icon-menu"></i>
-      <span slot="title">导航二</span>
-    </el-menu-item>
-    <el-menu-item index="3" disabled>
-      <i class="el-icon-document"></i>
-      <span slot="title">导航三</span>
-    </el-menu-item>
-    <el-menu-item index="4">
-      <i class="el-icon-setting"></i>
-      <span slot="title">导航四</span>
-    </el-menu-item>
-  </el-menu>
+    <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose"
+        :collapse="isCollapse">
+        <el-menu-item v-for="item in noChildren" :key="item.name" :index="item.name">
+            <i :class="`el-icon-${item.icon}}`"></i>
+            <span slot="title">{{ item.label }}</span>
+        </el-menu-item>
+
+        <el-submenu v-for="item in hasChildren" :key="item.label" :index="item.name">
+            <template slot="title">
+                <i :class="`el-icon-${item.icon}}`"></i>
+                <span slot="title">{{ item.label }}</span>
+            </template>
+            <el-menu-item-group v-for="subItem in item.children" :key="subItem.path">
+                <el-menu-item :index="subItem.path">{{subItem.label}}</el-menu-item>
+            </el-menu-item-group>
+        </el-submenu>
+    </el-menu>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      isCollapse: true
-    };
-  },
-  methods: {
-    handleOpen(key, keyPath) {
-      console.log(key, keyPath);
+    data() {
+        return {
+            isCollapse: false,
+            menuData: [
+                {
+                    path: '/',
+                    name: 'home',
+                    label: '首页',
+                    icon: 's-home',
+                    url: 'home/home'
+                },
+                {
+                    path: '/mall',
+                    name: 'mall',
+                    label: '商品管理',
+                    icon: 'video-play',
+                    url: 'MallManage/MallManage'
+                },
+                {
+                    path: '/user',
+                    name: 'user',
+                    label: '用户管理',
+                    icon: 'user',
+                    url: 'UserManage/UserManage'
+                },
+                {
+                    label: '其他',
+                    icon: 'location',
+                    children: [
+                        {
+                            path: '/page1',
+                            name: 'page1',
+                            label: '页面1',
+                            icon: 'setting',
+                            url: 'other/pageOne'
+                        },
+                        {
+                            path: '/page2',
+                            name: 'page2',
+                            label: '页面2',
+                            icon: 'setting',
+                            url: 'other/pageTwo'
+                        }
+                    ]
+                },
+            ]
+        };
     },
-    handleClose(key, keyPath) {
-      console.log(key, keyPath);
+    methods: {
+        handleOpen(key, keyPath) {
+            console.log(key, keyPath);
+        },
+        handleClose(key, keyPath) {
+            console.log(key, keyPath);
+        }
+    },
+    computed: {
+        //1、没有子菜单的
+        noChildren() {
+            return this.menuData.filter(item => !item.children)
+        },
+        //2、有子菜单的
+        hasChildren() {
+            return this.menuData.filter(item => item.children)
+
+        }
     }
-  }
 }
 </script>
 
 <style>
 .el-menu-vertical-demo:not(.el-menu--collapse) {
-  width: 200px;
-  min-height: 400px;
+    width: 200px;
+    min-height: 400px;
 }
 </style>
